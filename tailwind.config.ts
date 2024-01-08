a190ea9514a0ac94994e2931ee007f7d2ec7845f
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-08 17:41:09 +0000

    Init Next.js app

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+import theme from 'tailwindcss/defaultTheme';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...theme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
