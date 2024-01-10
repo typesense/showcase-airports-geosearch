be78860eb39b8b658a09325412c73ecff7484a77
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-10 20:06:39 +0000

    Add FieldLabel component

---
+++ src/components/FieldLabel.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+interface FieldLabelProps {
+  children: ReactNode;
+}
+
+export default function FieldLabel({ children }: FieldLabelProps) {
+  return (
+    <span className="absolute uppercase text-[10px] tracking-wider font-medium top-2.5 left-6 text-gray-600">
+      {children}
+    </span>
+  );
+}
