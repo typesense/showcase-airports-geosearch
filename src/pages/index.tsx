b7a155c4af56b4c6ed6f855614aabf286b5bf7f1
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-10 20:17:32 +0000

    Add full-screen Google Maps on the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,38 @@
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import mapStyle from '@/lib/map-style';
+
+function Map() {
+  return (
+    <GoogleMap
+      mapContainerClassName="w-full h-full"
+      zoom={14}
+      center={{
+        lat: -3.745,
+        lng: -38.523,
+      }}
+      options={{
+        styles: mapStyle,
+        streetViewControl: false,
+        zoomControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        draggableCursor: 'grab',
+        draggingCursor: 'grabbing',
+      }}
+    ></GoogleMap>
+  );
+}
+
 export default function Home() {
-  return <main>Home</main>;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <div className="w-screen h-screen">
+      <Map />
+    </div>
+  );
 }
