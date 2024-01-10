cc9615b97e8d5577c23b9362c1d4046ab95e6eac
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-10 20:13:46 +0000

    Add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Checkbox } from './Checkbox';
+export { default as FieldLabel } from './FieldLabel';
+export { default as Popover } from './Popover';
+export * from './RangeSlider';
+export { default as Switch } from './Switch';
