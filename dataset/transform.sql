c7f51645f99546ad07a28304f538d5e5ad8788f9
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-17 19:33:52 +0000

    Include number of runways in airports and randomize their order

--- dataset/transform.sql
+++ dataset/transform.sql
@@ -2,71 +2,45 @@
 .import ./source/airports.csv airports
 .import ./source/countries.csv countries
 .import ./source/regions.csv regions
+.import ./source/runways.csv runways
 
 .mode columns
 .headers ON
 
-ALTER TABLE 
-  countries RENAME COLUMN code TO iso_country;
-ALTER TABLE 
-  countries RENAME COLUMN name TO country_name;
-ALTER TABLE 
-  countries RENAME COLUMN continent TO country_continent;
-ALTER TABLE 
-  regions RENAME COLUMN code TO iso_region;
-ALTER TABLE 
-  regions RENAME COLUMN name TO region_name;
-ALTER TABLE 
-  regions RENAME COLUMN continent TO region_continent;
-
-CREATE VIEW out AS 
-SELECT 
-  * 
+CREATE VIEW out AS
+SELECT
+  *
 FROM 
-  airports 
+  airports
   INNER JOIN (
-    SELECT 
-      iso_country, 
-      country_name, 
-      country_continent 
-    FROM 
+    SELECT
+      code AS iso_country,
+      name AS country_name
+    FROM
       countries
-  ) AS countries ON airports.iso_country = countries.iso_country 
+  ) AS countries ON airports.iso_country = countries.iso_country
   INNER JOIN (
-    SELECT 
-      iso_region, 
-      region_name, 
-      region_continent 
-    from 
+    SELECT
+      code AS iso_region,
+      name AS region_name
+    FROM
       regions
-  ) AS regions ON airports.iso_region = regions.iso_region;
-
+  ) AS regions ON airports.iso_region = regions.iso_region
+  INNER JOIN (
+    SELECT
+      airport_ident AS ident,
+      COUNT(*) AS num_runways
+    FROM
+      runways
+    WHERE closed = 0
+    GROUP BY
+      ident
+  ) AS runways ON airports.ident = runways.ident
+ORDER BY RANDOM();
 
 .mode csv
 .output data.csv
 
-SELECT 
-  id, 
-  ident, 
-  type, 
-  name, 
-  latitude_deg, 
-  longitude_deg, 
-  elevation_ft, 
-  continent, 
-  iso_country, 
-  iso_region, 
-  municipality, 
-  scheduled_service, 
-  gps_code, 
-  iata_code, 
-  local_code, 
-  home_link, 
-  wikipedia_link, 
-  keywords, 
-  country_name, 
-  region_name 
-FROM 
-  out;
+SELECT * FROM out;
 
 .output stdout
