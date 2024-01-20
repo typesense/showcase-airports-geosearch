de654ae29f76dc6f4f46827673ca14046a6366c6
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-20 13:03:00 +0000

    Add links to other pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { InstantSearch } from 'react-instantsearch';
+import { ArrowSquareOut, GithubLogo } from '@phosphor-icons/react';
 import Head from 'next/head';
+import { InstantSearch } from 'react-instantsearch';
 
 import {
   AirportTypeControl,
@@ -7,6 +8,7 @@ import {
   Map,
   ScheduledServiceControl,
   SearchControl,
+  SearchStats,
 } from '@/components';
 import typesense from '@/lib/typesense';
 
@@ -18,11 +20,38 @@ export default function Home() {
       </Head>
       <InstantSearch searchClient={typesense.searchClient} indexName="airports">
         <div className="flex flex-col w-screen h-screen antialiased">
-          <div className="bg-white drop-shadow relative z-10">
-            <div className="px-6 py-5 border-b">
+          <div className="bg-white drop-shadow relative z-10 divide-y">
+            <div className="flex items-center gap-4 px-6 py-4">
               <h1 className="text-2xl font-serif font-black">
                 Airports Search
               </h1>
+
+              <div className="flex gap-1.5 items-center text-sm">
+                Powered by
+                <a href="https://typesense.org" target="_blank">
+                  <img src="/images/typesense.svg" className="h-5" />
+                </a>
+              </div>
+
+              <div className="flex items-center gap-2 ml-auto">
+                <a
+                  href="#"
+                  target="_blank"
+                  className="btn-secondary px-4 py-2 text-sm"
+                >
+                  About
+                  <ArrowSquareOut weight="bold" className="w-4 h-4 -mt-0.5" />
+                </a>
+
+                <a
+                  href="#"
+                  target="_blank"
+                  className="btn-secondary px-4 py-2 text-sm"
+                >
+                  Source Code
+                  <GithubLogo weight="bold" className="w-4 h-4" />
+                </a>
+              </div>
             </div>
             <div className="grid grid-cols-[1fr,1fr,1fr,auto] divide-x">
               <SearchControl />
@@ -30,6 +59,7 @@ export default function Home() {
               <ElevationControl />
               <ScheduledServiceControl />
             </div>
+            <SearchStats />
           </div>
           <Map />
         </div>
