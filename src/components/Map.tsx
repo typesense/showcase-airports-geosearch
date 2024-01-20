d6b0b441bb7dd1d66a1b2f01cc82c0285fff5fec
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-20 11:08:21 +0000

    Fix map panning on airport selection covering part of the popover

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -35,6 +35,7 @@ function MapResults() {
             onClick={() => {
               if (!map) return;
               map.panTo({ lat: hit.location[0], lng: hit.location[1] });
+              map.panBy(0, -100);
             }}
           >
             <AirportMarker
