f6208cf7871c978442b85ed842d928219ea9ad7f
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-20 11:09:03 +0000

    Enable antialiasing on fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ import typesense from '@/lib/typesense';
 export default function Home() {
   return (
     <InstantSearch searchClient={typesense.searchClient} indexName="airports">
-      <div className="flex flex-col w-screen h-screen">
+      <div className="flex flex-col w-screen h-screen antialiased">
         <div className="bg-white drop-shadow relative z-10">
           <div className="px-6 py-5 border-b">
             <h1 className="text-2xl font-serif font-black">Airports Search</h1>
