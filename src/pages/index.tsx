c5241705a66ccc0714049b3c384a3f5308195914
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-01-20 11:14:42 +0000

    Update page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { InstantSearch } from 'react-instantsearch';
+import Head from 'next/head';
 
 import {
   AirportTypeControl,
@@ -11,21 +12,28 @@ import typesense from '@/lib/typesense';
 
 export default function Home() {
   return (
-    <InstantSearch searchClient={typesense.searchClient} indexName="airports">
-      <div className="flex flex-col w-screen h-screen antialiased">
-        <div className="bg-white drop-shadow relative z-10">
-          <div className="px-6 py-5 border-b">
-            <h1 className="text-2xl font-serif font-black">Airports Search</h1>
+    <>
+      <Head>
+        <title>Airports Search with Typesense</title>
+      </Head>
+      <InstantSearch searchClient={typesense.searchClient} indexName="airports">
+        <div className="flex flex-col w-screen h-screen antialiased">
+          <div className="bg-white drop-shadow relative z-10">
+            <div className="px-6 py-5 border-b">
+              <h1 className="text-2xl font-serif font-black">
+                Airports Search
+              </h1>
+            </div>
+            <div className="grid grid-cols-[1fr,1fr,1fr,auto] divide-x">
+              <SearchControl />
+              <AirportTypeControl />
+              <ElevationControl />
+              <ScheduledServiceControl />
+            </div>
           </div>
-          <div className="grid grid-cols-[1fr,1fr,1fr,auto] divide-x">
-            <SearchControl />
-            <AirportTypeControl />
-            <ElevationControl />
-            <ScheduledServiceControl />
-          </div>
+          <Map />
         </div>
-        <Map />
-      </div>
-    </InstantSearch>
+      </InstantSearch>
+    </>
   );
 }
